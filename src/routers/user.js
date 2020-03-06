37799b6724cd5430b91f01cb80a84614c1d90263
Ruslan Bondar <mailofbondik@gmail.com>
2020-03-06 17:05:37 +0200

    sender removed

--- src/routers/user.js
+++ src/routers/user.js
@@ -118,58 +118,58 @@ router.delete("/users/me", auth, async (req, res) => {
 //   },
 // });
 
-const upload = multer({
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error("Please upload an image"));
-    }
+// const upload = multer({
+//   limits: {
+//     fileSize: 1000000
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//       return cb(new Error("Please upload an image"));
+//     }
 
-    cb(undefined, true);
-  }
-});
+//     cb(undefined, true);
+//   }
+// });
 
-router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
-  req.user.avatar = req.file.buffer;
-  await req.user.save();
-  res.send();
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-})
+// router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
+//   req.user.avatar = req.file.buffer;
+//   await req.user.save();
+//   res.send();
+// }, (error, req, res, next) => {
+//   res.status(400).send({ error: error.message })
+// })
 
-router.post(
-  "/users/me/avatar",
-  auth,
-  upload.single("avatar"),
-  async (req, res) => {
-    const buffer = await sharp(req.file.buffer)
-      .resize({ width: 250, height: 250 })
-      .png()
-      .toBuffer();
-    req.user.avatar = buffer;
-    await req.user.save();
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-  }
-);
+// router.post(
+//   "/users/me/avatar",
+//   auth,
+//   upload.single("avatar"),
+//   async (req, res) => {
+//     const buffer = await sharp(req.file.buffer)
+//       .resize({ width: 250, height: 250 })
+//       .png()
+//       .toBuffer();
+//     req.user.avatar = buffer;
+//     await req.user.save();
+//     res.send();
+//   },
+//   (error, req, res, next) => {
+//     res.status(400).send({ error: error.message });
+//   }
+// );
 
-router.get("/users/:id/avatar", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
+// router.get("/users/:id/avatar", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
 
-    if (!user || !user.avatar) {
-      throw new Error();
-    }
+//     if (!user || !user.avatar) {
+//       throw new Error();
+//     }
 
-    res.set("Content-Type", "image/png");
-    res.send(user.avatar);
-  } catch (e) {
-    res.status(404).send();
-  }
-});
+//     res.set("Content-Type", "image/png");
+//     res.send(user.avatar);
+//   } catch (e) {
+//     res.status(404).send();
+//   }
+// });
 
 module.exports = router;
