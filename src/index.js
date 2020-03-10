0da06b441bf398cab2f0826f58ca1d4da0eb196e
Ruslan Bondar <mailofbondik@gmail.com>
2020-03-10 15:29:10 +0200

    code refactor

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-const multer = require('multer')
-const cors = require('cors')
-const port = process.env.PORT || 3001
-const app = express()
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+const multer = require("multer");
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+const app = express();
 app.use(cors());
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port', port)
-})
+  console.log("Server is up on port", port);
+});
 
-const Task = require('./models/task')
-const User = require('./models/user')
+const Task = require("./models/task");
+const User = require("./models/user");
 
 const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+  // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+  // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+  // await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
+};
 
-    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
+main();
