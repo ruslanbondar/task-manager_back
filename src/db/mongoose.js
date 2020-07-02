ee51119f0f62cc3ffe4e63ac010f1860ef0b9d36
Ruslan Bondar <mailofbondik@gmail.com>
2020-07-02 21:19:31 +0300

    swagger config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 mongoose.connect(
-  "mongodb+srv://Ruslan:Bondik25cur@cluster0-t9zc4.mongodb.net/task-manager?retryWrites=true&w=majority",
+  'mongodb+srv://Ruslan:Bondik25cur@cluster0-t9zc4.mongodb.net/task-manager?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   }
-);
+)
